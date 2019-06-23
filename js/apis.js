2082674edbffd1a50ba7c1a4873c025576eab084
anita86 <analiasoledad1986@gmail.com>
2019-06-23 03:47:53 -0300

    paso 2: crear mapa > Finalizado

--- js/apis.js
+++ js/apis.js
@@ -1,12 +1,16 @@
 var mapa // Mapa que vamos a modificar
 
 /* Crear la variable posicionCentral con las coordenadas donde se va a centrar el mapa */
-
+var posicionCentral = {lat: -34.60665, lng: -58.434619} ;
 // Inicializa el mapa con un valor de zoom y una locación en el medio
 function inicializarMapa () {
     /* Modificá la variable mapa con el constructor Map().
     Tendrás que asignarle un valor de zoom y
     un centro igual a la variable posicionCentral. */
+    mapa = new google.maps.Map(document.getElementById("map"), {
+    	center: {lat: -34.60665, lng: -58.434619},
+    	zoom: 15,
+    });
 
   geocodificadorModulo.inicializar()
   marcadorModulo.inicializar()
