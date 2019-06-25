e394b29b49698ebf5e99bd9827d6e71487a4059a
anita86 <analiasoledad1986@gmail.com>
2019-06-25 11:38:45 -0300

    Guia 3 paso 1 completo

--- js/streetView.js
+++ js/streetView.js
@@ -3,7 +3,18 @@ streetViewModulo = (function () {
 
   function inicializar () {
         /* Completar la función inicializar()  que crea un panorama
-        en una posición y lo muestra en la página. */
+        en una posición y lo muestra en la página.
+        Debe crear una instancia de StreetViewPanorama con una posición, 
+        el elemento HTML donde se mostrará el mapa. */
+        panorama = new google.maps.StreetViewPanorama(
+        document.getElementById('pano'), {
+          position: posicionCentral,
+          pov: {
+            heading: 34,
+            pitch: 10
+          }
+        });
+
 
   }
 
@@ -12,6 +23,13 @@ streetViewModulo = (function () {
         /* Completar la función fijarStreetView que actualiza la posición
          de la variable panorama y cambia el mapa de modo tal que se vea
          el streetView de la posición actual. */
+
+         //Pista: La clase StreetViewPanorama y un método de la clase Map 
+         //te servirán para cumplir la consigna. Lee sobre estos en la documentación
+         panorama.setPosition(ubicacion);
+
+               mapa.setStreetView(panorama);
+
   }
 
   return {
